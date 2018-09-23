1ec837ab01c60a7fcd1cecc5ca054df884c6924e
Geek Different <geekdifferentidea@gmail.com>
2018-09-24 05:55:07 +0900

    Initial setup

---
+++ youtube_JTBC_newsroom.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+#This playlist is Nightly News.
+PLAYLIST="https://www.youtube.com/playlist?list=PL3Eb1N33oAXhNHGe-ljKHJ5c0gjiZkqDk"
+BASE_URL="https://www.youtube.com/watch?v="
+BASE_STRING="뉴스룸"
+
+#Write your download directory.
+#DOWN_DIR="/home/myhone/video"
+DOWN_DIR=$HOME
+
+TMP_DIR=$DOWN_DIR"/_tmp_JTBC_newsroom"
+MAX_DW="1"
+
+OIFS="$IFS"
+IFS=$'\n'
+
+cd $DOWN_DIR
+mkdir $TMP_DIR
+
+mv *$BASE_STRING* $TMP_DIR
+
+for ID in `youtube-dl --dateafter now-1week --match-title $BASE_STRING --max-downloads $MAX_DW $PLAYLIST --get-id `; do
+    URL=$BASE_URL$ID
+    FILE=`youtube-dl --get-filename -o '%(title)s.%(ext)s' $URL`
+
+    if [ -f "$TMP_DIR/$FILE" ]; then
+        echo "File [$FILE] exists."
+        mv $TMP_DIR/$FILE $TMP_DIR/..
+    else
+        echo "File [$FILE] does not exists."
+        youtube-dl -f 'best' -o $DOWN_DIR/'%(title)s.%(ext)s' $URL
+    fi
+done
+
+rm -drf $TMP_DIR
+IFS="$OIFS"
+
+exit 1
---
+++ youtube_nightly_news.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+#This playlist is Nightly News.
+PLAYLIST="https://www.youtube.com/playlist?list=PL0tDb4jw6kPyGy4AIjirldbeP6nxpWGKU"
+BASE_URL="https://www.youtube.com/watch?v="
+BASE_STRING="Nightly News Broadcast"
+
+#Write your download directory.
+#DOWN_DIR="/home/myhone/video"
+DOWN_DIR=$HOME
+
+TMP_DIR=$DOWN_DIR"/_tmp_nightly_news"
+MAX_DW="3"
+
+OIFS="$IFS"
+IFS=$'\n'
+
+cd $DOWN_DIR
+mkdir $TMP_DIR
+
+mv *$BASE_STRING* $TMP_DIR
+
+for ID in `youtube-dl --dateafter now-1week --match-title $BASE_STRING --max-downloads $MAX_DW $PLAYLIST --get-id `; do
+    URL=$BASE_URL$ID
+    FILE=`youtube-dl --get-filename -o '%(title)s.%(ext)s' $URL`
+
+    if [ -f "$TMP_DIR/$FILE" ]; then
+        echo "File [$FILE] exists."
+        mv $TMP_DIR/$FILE $TMP_DIR/..
+    else
+        echo "File [$FILE] does not exists."
+        youtube-dl -f 'best' -o $DOWN_DIR/'%(title)s.%(ext)s' $URL
+    fi
+done
+
+rm -drf $TMP_DIR
+IFS="$OIFS"
+
+exit 1
